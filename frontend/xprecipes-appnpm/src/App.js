5a29374079704720b59f84abc4bd4b6486c26177
j2002chen <j997chen@uwaterloo.ca>
2023-01-17 23:25:05 -0500

    implemented hook for login

--- frontend/xprecipes-appnpm/src/App.js
+++ frontend/xprecipes-appnpm/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import Dashboard from './components/dashboard/Dashboard';
+import Preferences from './components/preferences/Preferences';
+import NotFound from './components/notfound/NotFound';
+import Login from './components/Login/Login'
+import useToken from './components/app/useToken';
+
+
+
 
 function App() {
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes path='/' fallback={<NotFound />}>
+          <Route path='dashboard' element={<Dashboard />} />     
+          <Route path='preferences' element={<Preferences />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
